dc3918ad0b3bfcaa778ec0bee7a3ba26f3e5102a
Jaime Tanori <jaime.tanori@gmail.com>
2016-11-21 22:47:00 -0700

    Stage env file

--- www/js/env.js
+++ www/js/env.js
@@ -1,8 +1,8 @@
 window.app
 
-.constant('API_URL', 'http://localhost:5001')
-.constant('APP_ID', 'jound')
-.constant('JS_KEY', 'jound-js')
+.constant('API_URL', 'http://api-stage.jound.mx')
+.constant('APP_ID', '2d42ca0c-8ca7-40e4-a8f7-b5a09b07a96c')
+.constant('JS_KEY', 'f2673746-8a8e-499e-9cfa-3c9194b8ffaa')
 .constant('GOOGLE_MAPS_API_URL', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDlo3eTkN9_-X-z8ps-pcZgAcMKEELdI8s&libraries=places,geometry')
 .constant('GOOGLE_MAPS_API_KEY', 'AIzaSyDlo3eTkN9_-X-z8ps-pcZgAcMKEELdI8s')
 .constant('PUBNUB_PUBLISH_KEY', 'pub-c-3347ea90-98c9-465f-b5bf-d4ff2751214e')
